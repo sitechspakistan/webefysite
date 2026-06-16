0face48c29388c5f57b78180555e340ef7bf3e40
SyedMMurtuza <syedmmurtuza393@gmail.com>
2026-06-16 16:09:01 +0500

    update

--- src/app/products/ai-booking-agent/page.js
+++ src/app/products/ai-booking-agent/page.js
@@ -1,11 +1,9 @@
 import AiAgent from '@/app/component/AiAgent'
 import BookingAgent from '@/app/component/BookingAgent'
 import CalendarTools from '@/app/component/CalendarTools'
-import ContactForm from '@/app/component/ContactForm'
 import PerfectIndustry from '@/app/component/PerfectIndustry'
 import ProductFrom from '@/app/component/ProductFrom'
 import ThirdHero from '@/app/component/ThirdHero'
-import React from 'react'
 
 export const metadata = {
     title: "AI Booking Agent | Webefy Today",
