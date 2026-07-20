3f4779feacbe6052c0253b3595c27632041bf7b4
webefydesign <webefydesign@gmail.com>
2026-07-21 01:29:42 +0500

    Update page.js

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -92,7 +92,7 @@ export default async function Page({ params }) {
                                 dangerouslySetInnerHTML={{ __html: project.content }}
                             />
                         </div>
-                        {/* <AdSense /> */}
+                        <AdSense />
                     </div>
 
 
